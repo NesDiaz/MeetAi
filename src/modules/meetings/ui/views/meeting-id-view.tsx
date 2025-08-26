019297cb204bb39eef397516cc42f50f8a5ac987
Vanessa <dnessa92@yahoo.com>
2025-08-26 14:55:39 -0700

    21: meeting variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -12,6 +12,10 @@ import { toast } from "sonner";
 import { useConfirm } from "@/hooks/use-confirm";
 import { useState } from "react";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 
 interface Props {
@@ -52,7 +56,13 @@ const { data } = useSuspenseQuery(trpc.meetings.getOne.queryOptions({ id: meetin
 
     await removeMeeting.mutateAsync({ id: meetingId });
   };
- 
+
+  const isActive = data.status === "active";
+  const isUpcoming = data.status === "upcoming";
+  const isCancelled= data.status === "cancelled";
+  const isCompleted = data.status === "completed";
+  const isProcessing = data.status === "processing";
+
   return (
     <>
     <RemoveConfirmation />
@@ -69,7 +79,15 @@ const { data } = useSuspenseQuery(trpc.meetings.getOne.queryOptions({ id: meetin
         onEdit={() => setUpdateMeetingDialogOpen(true)}
        onRemove={handleRemoveMeeting}
       />
-
+        {isCancelled && <CancelledState />}
+        {isCompleted && <div>completed</div>}
+        {isProcessing && <ProcessingState />}
+        {isActive && <ActiveState meetingId={meetingId} />}
+        {isUpcoming && <UpcomingState 
+            meetingId={meetingId}
+            onCancelMeeting={() => {}}
+            isCancelling={false}
+        />}
     </div>
     </>
 );
