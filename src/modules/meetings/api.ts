6d34aff40d023f36a10d6dcaa6ce250a624aaf8e
Vanessa <dnessa92@yahoo.com>
2025-09-09 16:34:11 -0700

    24: Background Jobs (Inngest, OpenAI )

---
+++ src/modules/meetings/api.ts
@@ -0,0 +1,11 @@
+// modules/meetings/api.ts
+// export async function getMeetings({ page }: { page: number }) {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/meetings?page=${page}`, {
+//       cache: "no-store", // ensures fresh data
+//     });
+  
+//     if (!res.ok) throw new Error("Failed to fetch meetings");
+//     return res.json();
+//   }
+  
+  // error testing page
