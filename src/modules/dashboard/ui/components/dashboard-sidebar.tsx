4ebdb2f9cf097f49e3c4cfdc8fee53859302d071
Vanessa <dnessa92@yahoo.com>
2025-11-08 16:23:17 -0800

    Resolved merge conflicts

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -64,6 +64,7 @@ const pathname = usePathname();
                                 asChild
                                 className={cn(
                                     "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 hover:bg-gradient-to-r from-sidebar-accent from-5% via-sidebar/50 via-30% to-sidebar/50",
+                                    "h-10 border border-transparent hover:border-[#5D6B68]/10 hover:bg-linear-to-r from-sidebar-accent from-5% via-sidebar/50 via-30% to-sidebar/50",
                                     pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                                 )}
                                     isActive={pathname === item.href}
@@ -91,7 +92,7 @@ const pathname = usePathname();
                                 <SidebarMenuButton 
                                 asChild
                                 className={cn(
-                                    "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 hover:bg-gradient-to-r from-sidebar-accent from-5% via-sidebar/50 via-30% to-sidebar/50",
+                                    "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 hover:bg-linear-to-r from-sidebar-accent from-5% via-sidebar/50 via-30% to-sidebar/50",
                                     pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                                 )}
                                     isActive={pathname === item.href}
